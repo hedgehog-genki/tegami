138daf433728a93c925f2fa92b7f7de8f24ab765
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-07-18 17:43:56 +0900

    募集期間の掲載

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.181.13.194', user: 'ec2-user', roles: %w{app db web}
+server '54.95.48.109', user: 'ec2-user', roles: %w{app db web}
